a2769d90cfa4923c0a55f03d8bdf570b9aea0bd4
Andreas <ageorgiou4@uclan.ac.uk>
2021-01-11 14:07:56 +0200

    foodbank fragments categories list

--- app/src/main/java/com/example/foodbank/ui/categories/CategoriesFragment.java
+++ app/src/main/java/com/example/foodbank/ui/categories/CategoriesFragment.java
@@ -1,12 +1,10 @@
 package com.example.foodbank.ui.categories;
 
-import android.content.res.ColorStateList;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.GridView;
 import android.widget.TextView;
 
@@ -27,26 +25,19 @@ public class CategoriesFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-
-
         categoriesViewModel =
                 new ViewModelProvider(this).get(CategoriesViewModel.class);
         View root = inflater.inflate(R.layout.a2_fragment_categories, container, false);
-        View gridcell = inflater.inflate(R.layout.a2_fragment_categories, container, false);
-
-
-
+        GridView gridView = (GridView) root.findViewById(R.id.gridView_categories);
 
         String[] gridViewCategories = {"Breakfast Today, tomorrow hello world", "Lunch", "Dinner", "Afternoon"};
-        GridView gridView = (GridView) root.findViewById(R.id.gridView_categories);
 
         List<String> values = new ArrayList<String>();
         for (int y = 0; y < gridViewCategories.length; y++) {
             values.add(gridViewCategories[y]);
         }
+
         gridView.setAdapter(new ArrayAdapter<String>(getActivity(), R.layout.a2_gridcell, values));
-
-
 
         final TextView textView = root.findViewById(R.id.text_categories);
         categoriesViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
